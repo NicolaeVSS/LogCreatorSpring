604bad262e4e4e03258fb0d53eb013baacb3731f
NicolaeVSS <nicolae.vasile@smoothstack.com>
2020-01-07 12:45:45 -0500

    unusedimport and spacing

--- src/main/java/com/ss/logcreator/controller/LogCreatorController.java
+++ src/main/java/com/ss/logcreator/controller/LogCreatorController.java
@@ -1,7 +1,5 @@
 package com.ss.logcreator.controller;
 
-import java.time.Instant;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -12,12 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/*")
 public class LogCreatorController {
-	
+
 	Logger logger = LoggerFactory.getLogger(LogCreatorController.class);
-	
+
 	@GetMapping(path = "")
-	public HttpStatus isUp() 
-	{
+	public HttpStatus isUp() {
 		logger.info(HttpStatus.OK.toString());
 		return HttpStatus.OK;
 	}
