d710dd87a2bd36b93157fc2b8508c7c6f497b280
NicolaeVSS <nicolae.vasile@smoothstack.com>
2020-01-09 13:51:21 -0500

    will now output varied logs

--- src/main/java/com/ss/logcreator/controller/LogCreatorController.java
+++ src/main/java/com/ss/logcreator/controller/LogCreatorController.java
@@ -1,5 +1,7 @@
 package com.ss.logcreator.controller;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -12,10 +14,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class LogCreatorController {
 
 	Logger logger = LoggerFactory.getLogger(LogCreatorController.class);
+	Random random = new Random();
 
 	@GetMapping(path = "")
 	public HttpStatus isUp() {
-		logger.info(HttpStatus.OK.toString());
-		return HttpStatus.OK;
+		HttpStatus status;
+		
+		// HALF OF THE TIME IT WILL BE OK, A QUARTER 404, A QUARTER 500
+		switch( random.nextInt(4) ) {
+			case 0:
+				status = HttpStatus.OK;
+				logger.info(status.toString());
+				break;
+			case 1:
+				status = HttpStatus.OK;
+				logger.info(status.toString());
+				break;
+			case 2:
+				status = HttpStatus.NOT_FOUND;
+				logger.warn(status.toString());
+				break;
+			case 3:
+				status = HttpStatus.INTERNAL_SERVER_ERROR;
+				logger.error(status.toString());
+				break;
+			default: // will never be reached
+				status = HttpStatus.OK;
+				logger.info(status.toString());
+				break;
+		}
+		
+		return status;
 	}
 }
